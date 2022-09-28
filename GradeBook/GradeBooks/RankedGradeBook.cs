f65487e2f9d6684e795ce85dc2450e376e5251f3
Thanh Duong <43357117+tduoth@users.noreply.github.com>
2022-09-28 23:40:20 +0000

    Update RankedGradeBook.cs

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using GradeBook.Enums;
 
 namespace GradeBook.GradeBooks
@@ -8,5 +9,23 @@ namespace GradeBook.GradeBooks
         {
             Type = GradeBookType.Ranked;
         }
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+                throw new System.InvalidOperationException("Ranked grading requires at least 5 students.");
+            var threshold = (int)System.Math.Ceiling(Students.Count * 0.2);
+             var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
+            if (grades[threshold - 1] <= averageGrade)
+            return 'A';
+            else if ( grades[(threshold*2)-1] <= averageGrade)
+            return 'B';
+              else if ( grades[(threshold*3)-1] <= averageGrade)
+            return 'C';
+              else if ( grades[(threshold*4)-1] <= averageGrade)
+            return 'D';
+            else 
+            return 'F';
+            
+         }
     }
 }
