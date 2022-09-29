e47682d2f83f10d8548bf81c33d0db7c119a4b3d
Thanh Duong <43357117+tduoth@users.noreply.github.com>
2022-09-29 02:16:28 +0000

    Update StartingUserInterface.cs

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -36,26 +36,26 @@ namespace GradeBook.UserInterfaces
             var parts = command.Split(' ');
                    if (parts.Length != 4)
             {
-                Console.WriteLine("Command not valid, Create requires a name, type of gradebook, if it's weighted (true/false).");
+                Console.WriteLine("Command not valid, Create requires a name, type of gradebook, if it's weighted (true / false).");
                 return;
             }
             var name = parts[1];
             var type = parts[2].ToLower();
-            var weighted = bool.Parse(parts[3]);
-            BaseGradeBook gradeBook; 
-            if(type == "standard")
-            
-            gradeBook = new StandardGradeBook(name, weighted);
-            else if(type == "ranked")
-                gradeBook = new RankedGradeBook(name, weighted);
-            else 
-            else 
-            {
-                Console.WriteLine(" {0} is not a supported type of gradebook, please try again.", type);
-            return; 
-            }
-            Console.WriteLine("Created gradebook {0}.", name, weighted);
-            
+             var weighted = false;
+             if (parts[3] == "true")
+                 weighted = true;
+
+             BaseGradeBook gradeBook;
+             if (type == "standard")
+                 gradeBook = new StandardGradeBook(name, weighted);
+             if (type == "ranked")
+                 gradeBook = new RankedGradeBook(name, weighted);
+             else
+             {
+                 Console.WriteLine("{0} is not a supported type of gradebook, please try again",type);
+                 return;
+             }
+
         
             GradeBookUserInterface.CommandLoop(gradeBook);
         }
